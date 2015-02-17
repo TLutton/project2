8f0e87663ce41ec22ea50693bde55de71332982e
tlutton1 <tommylutton@gmail.com>
2015-02-17 07:42:54 +0000

    passing the correct port

--- src/client.cpp
+++ src/client.cpp
@@ -307,8 +307,8 @@ void Client::sendTrackerRequest()
 	int thePort = 0;
 	thePort += trRequest.getPort();
 	char strPort[6];
-	std::itoa(thePort, strPort);
-	if ((status = getaddrinfo(trRequest.getHost().c_str(), &strPort , &hints, &res)) != 0) 
+	itoa(thePort, strPort, 10);
+	if ((status = getaddrinfo(trRequest.getHost().c_str(), (const char*)&strPort , &hints, &res)) != 0) 
 	{
 		std::cerr << "getaddrinfo: " << gai_strerror(status) << std::endl;
 		return;
